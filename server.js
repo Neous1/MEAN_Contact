efe2c7d39b2f86aece89d6a54ed06ad79529cf82
Neous1 <Neous1@users.noreply.github.com>
2017-09-14 00:19:20 -0400

    use express to tell us where to look for the file

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var express = require("express");
 
 var app = express();
 
-app.get("/", function(req, res){ 
-    res.send("Hello world from server.js")
-});
+//.static tell express to look for static files in a public folder
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("Server running on port 3000")
