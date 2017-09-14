dcdd61fee6b02ae071677bdae1d02433e772b46d
Neous1 <Neous1@users.noreply.github.com>
2017-09-14 12:46:13 -0400

    add content to controller.js to be pulled and displayed on browser

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,4 +3,24 @@ var app = angular.module('myApp',[]);
 
 app.controller("AppCtrl", function ($scope) {
  console.log("hello from the controller")
+
+ person1 = {
+     name:"Tim",
+     email:"tim@email.com", 
+     number:"(111) 111-1111"
+ };
+ person2 = {
+     name:"Emily",
+     email:"emily@email.com", 
+     number:"(222) 222=2222"
+ };
+ person3 = {
+     name:"John",
+     email:"john@email.com", 
+     number:"(333) 333 3333"
+ };
+
+ var  contactList = [person1, person2, person3 ];
+
+ $scope.contactList = contactList
 });
