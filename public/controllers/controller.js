6af8e0725bbe22b5b5ba553b9ec8bab6877c8099
Neous1 <Neous1@users.noreply.github.com>
2017-09-15 12:52:47 -0400

    add refresh function to refresh page after adding contact and clear the input box

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,23 +3,27 @@ var app = angular.module('myApp', []);
 app.controller("AppCtrl", function ($scope, $http) {
     console.log("hello from the controller")
 
-
-    $http.get("/contactList")
-        .then(function (success) {
-            console.log("We ve got data")
-            $scope.contactList = success.data;
-        }, function(error){
+    var refresh = function () {
+        $http.get("/contactList")
+            .then(function (success) {
+                console.log("We ve got data")
+                $scope.contactList = success.data;
+                $scope.contact = null;//clear input box after we call refresh()                
+            }, function (error) {
                 console.log("We've got error");
 
-        });
-//addContact() sends input data to server
-    $scope.addContact = function(){
+            });
+    };
+    refresh();
+    //addContact() sends input data to server
+    $scope.addContact = function () {
         console.log($scope.contact);
         $http.post("/contactList", $scope.contact)
-        .then(function(success){
-            console.log(success.data)
-        }, function(error){
-            console.log("there is an error sending data back to controller")
-        })
+            .then(function (success) {
+                console.log(success.data)
+            }, function (error) {
+                console.log("there is an error sending data back to controller")
+            });
+            refresh();
     };
 });
